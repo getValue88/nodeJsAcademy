b87067a0d0e1efdc62680c52bdd853e5c87bba36
getValue88 <francisco.corti@globant.com>
2020-06-07 15:14:22 -0300

    load spinner animation for search and recipe sections added - search results pagination functionality added - recipe model, controller and view were created - calculate and update servings functionality added

--- week1/9-modernJs-ES6-npm-babel-webpack/src/js/index.js
+++ week1/9-modernJs-ES6-npm-babel-webpack/src/js/index.js
@@ -1,8 +1,10 @@
 // Global app controller
 // forkify-api.herokuapp.com --> api documentation
-import { elements } from './views/base';
 import Search from './models/Search';
+import Recipe from './models/Recipe';
+import { elements, renderLoader, clearLoader } from './views/base';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 
 //GLOBAL STATE
 //SEARCH OBJECT
@@ -10,6 +12,10 @@ import * as searchView from './views/searchView';
 //SHOPPING LIST OBJECT
 //LIKED RECIPES
 const state = {};
+
+/*
+*   SEARCH CONTROLLER
+*/
 
 const controlSearch = async () => {
     // 1- get query from view
@@ -22,23 +28,97 @@ const controlSearch = async () => {
         // 3- prepare ui for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
-        // 4- do the search
-        await state.search.getResults();
+        try {
+            // 4- do the search
+            await state.search.getResults();
 
-        // 5- render results on ui
-        searchView.renderResults(state.search.result);
+            // 5- render results on ui
+            clearLoader();
+            searchView.renderResults(state.search.result);
+
+        } catch (error) {
+            alert('Something wrong with the search...');
+        }
     }
-}
+};
 
+//search form - submit event handler
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+});
 
-})
+//pagination buttons event handler
+elements.searchResPages.addEventListener('click', (e) => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
 
 
+/*
+*   RECIPE CONTROLLER
+*/
 
-//recipe.js
-//const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+const controlRecipe = async () => {
+    // get ID from url
+    const id = window.location.hash.replace('#', '');
 
+    if (id) {
+        //prepare ui for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        //highlight selected item
+        if (state.search) searchView.highlightSelected(id);
+
+        //create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+            //get recipe data and parse ingredients
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+
+
+            //calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            //render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
+        } catch (error) {
+            alert('Error processing recipe!');
+
+        }
+    }
+};
+
+//hashchange and load window events handler
+['hashchange', 'load'].forEach(e => window.addEventListener(e, controlRecipe));
+
+
+// increment and decrement serving buttons in recipe click event handler
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        //decrase btn
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        //increase btn
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+
+    console.log(state.recipe);
+});
