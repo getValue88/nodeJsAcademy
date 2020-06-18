cac6f6a197aba2e05caf04046c3dd82e674d9d6a
getValue88 <francisco.corti@globant.com>
2020-06-18 20:48:33 -0300

    .

--- week3/task-manager/src/index.js
+++ week3/task-manager/src/index.js
@@ -17,5 +17,9 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+app.get('', (req, res) => {
+    res.send('Welcome to the Task Manager API');
+});
+
 //SERVER INIT
 app.listen(port, () => console.log('app listen on port ' + port));
