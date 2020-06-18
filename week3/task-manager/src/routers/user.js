cadac66fa5c1647ddeefdfdb5c4531c6db617504
getValue88 <francisco.corti@globant.com>
2020-06-18 13:53:39 -0300

    fetch avatar by user id

--- week3/task-manager/src/routers/user.js
+++ week3/task-manager/src/routers/user.js
@@ -130,4 +130,20 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     }
 });
 
+//get avatar by user id
+router.get('/users/:id/avatar', /* auth ,*/ async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+
+        res.set('Content-type', 'image/jpg');
+        res.send(user.avatar);
+    } catch (error) {
+        res.status(404).send();
+    }
+});
+
 module.exports = router;
