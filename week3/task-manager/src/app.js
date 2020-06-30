7a05aa3f4e2a2d206d32422dc80ded6908df71b9
getValue88 <francisco.corti@globant.com>
2020-06-30 17:05:15 -0300

    link to documentation was added in root path

--- week3/task-manager/src/app.js
+++ week3/task-manager/src/app.js
@@ -23,7 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.get('', (req, res) => {
-    res.send('Welcome to the Task Manager API');
+    res.send('Welcome to the Task Manager API<br><a href="/api-docs">Documentation</a>');
 });
 
 
